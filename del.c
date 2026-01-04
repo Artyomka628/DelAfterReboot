2c907b78b333eec25bcf0c577e2b7d7d45989ffd
Artyomka628 <promacrobit@gmail.com>
2026-01-04 15:41:23 +0300

    Fixed requirement for administrator rights

--- del.c
+++ del.c
@@ -232,17 +232,3 @@ int main(int argc, char** argv) {
     pathlist_free(&list);
     return 0;
 }
-
-__attribute__((section(".rsrc$01"), used))
-static const char manifest[] =
-"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
-"<assembly xmlns=\"urn:schemas-microsoft-com:asm.v1\" manifestVersion=\"1.0\">"
-"  <trustInfo xmlns=\"urn:schemas-microsoft-com:asm.v3\">"
-"    <security>"
-"      <requestedPrivileges>"
-"        <requestedExecutionLevel level=\"requireAdministrator\" uiAccess=\"false\"/>"
-"      </requestedPrivileges>"
-"    </security>"
-"  </trustInfo>"
-"</assembly>";
-
